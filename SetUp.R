1cb217b5a736a893cd2af20f8fd22514d0c8d5a3
Tim Dawes <tdawes2@isd.csc.mrc.ac.uk>
2018-09-11 12:43:13 +0100

    SetUp

--- SetUp.R
+++ SetUp.R
@@ -4,13 +4,14 @@
 
 # Load the relevant packages
 
-packages.needed<- c("Hmisc","colorRamps","FSA","pracma","gplots","pls","sf","smoothr","rowr")
-if (length(packages.needed)>1) {install.packages(packages.needed[is.na(match(packages.needed, rownames(installed.packages())))])}
-lapply(packages.needed, require, character.only = TRUE)
+    packages.needed<- c("Hmisc","colorRamps","FSA","pracma","gplots","pls","sf","smoothr","rowr")
+    packages.to.load<- packages.needed[!packages.needed %in% installed.packages()]
+    for (pack in packages.to.load) install.packages(pack, dependencies=TRUE)
+    lapply(packages.needed, require, character.only = TRUE)
 
 
 # Set up functions
 
-msd<- function(x) {return(list(MEAN=mean(x), SD=sd(x)))}
-miqr<- function(x) {return(list(MEDIAN=median(x), IQR=quantile(x, c(0.25, 0.75))))}
-calc.y<- function(x, coeffs) {return(coeffs[1]*x^3 + coeffs[2]*x^2 + coeffs[3]*x + coeffs[4])}
+    msd<- function(x) {return(list(MEAN=mean(x), SD=sd(x)))}
+    miqr<- function(x) {return(list(MEDIAN=median(x), IQR=quantile(x, c(0.25, 0.75))))}
+    calc.y<- function(x, coeffs) {return(coeffs[1]*x^3 + coeffs[2]*x^2 + coeffs[3]*x + coeffs[4])}
